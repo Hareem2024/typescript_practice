34f5d5cbc9c944bbffad2fc14e341d8a777ebd44
abdullahkhan9375 <abdullahkhan16021999@gmail.com>
2024-10-19 16:02:41 +1100

    problems

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,25 @@
-const hello: string = "hello world";
-console.log(hello);
+function substract(number1: number, number2: number): number {
+    console.log(number1)
+    console.log(number2)
+    return 0;
+}
+
+function sum(number1: number, number2: number): number
+{
+    console.log(number1)
+    console.log(number2)
+    return 0;
+}
+
+// Write a function fizzBuzz that prints the numbers from 1 to 100.
+// But for multiples of three, print "Fizz" instead of the number, and for the multiples of five, print "Buzz".
+// For numbers that are multiples of both three and five, print "FizzBuzz".
+function fizzBuzz(): void {}
+
+substract(0, 0);
+substract(5, 3);
+sum(0, 0);
+sum(4, 3);
+sum(1, 2);
+sum(3, 5);
+fizzBuzz()
